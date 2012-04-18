17066626a0983fa4ea2ccc630f2e3289dc8c29ed
Cody Duval <cody.duval@gmail.com>
2012-04-18 12:49:26 -0400

    Initial commit, using Rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECarmony::Application.config.secret_token = '1ef846dd5c7194ea198a7081bc5cc47947422810f3f11f7f8ce8a36f5b3b509a85e7862d2dd815f31de985875a966eae21645e90b213d8c21d1ad0482c3d1358'
